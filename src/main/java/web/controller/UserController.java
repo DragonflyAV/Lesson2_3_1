db775dfa79b9839429f6b80184ec8cf990202d51
Sanya <sasha141412@gmail.com>
2023-01-22 22:58:20 +0300

    v1

---
+++ src/main/java/web/controller/UserController.java
@@ -0,0 +1,72 @@
+package web.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import web.model.User;
+import web.services.UserService;
+
+import javax.validation.Valid;
+
+
+@Controller
+@RequestMapping("/")
+public class UserController {
+	private final UserService userService;
+
+	@Autowired
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
+
+	@GetMapping()
+	public String index(ModelMap model) {
+		model.addAttribute("users", userService.findAll());
+		return "index";
+	}
+
+	@GetMapping("/{id}")
+	public String show(@PathVariable("id") int id, ModelMap model) {
+		model.addAttribute("user", userService.findOne(id));
+		return "show";
+	}
+
+	@GetMapping("/new")
+	public String newPerson(@ModelAttribute("user") User user) {
+		return "new";
+	}
+
+	@PostMapping()
+	public String create(@ModelAttribute("user") @Valid User user, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return "new";
+		}
+		userService.save(user);
+		return "redirect:/";
+	}
+
+	@GetMapping("/{id}/edit")
+	public String edit(ModelMap model, @PathVariable("id") int id) {
+		model.addAttribute("user", userService.findOne(id));
+		return "edit";
+	}
+
+	@PatchMapping("/{id}")
+	public String update(@ModelAttribute("user") @Valid User user, BindingResult bindingResult,
+						 @PathVariable("id") int id) {
+		if (bindingResult.hasErrors()) {
+			return "edit";
+		}
+		userService.update(id, user);
+		return "redirect:/";
+	}
+
+	@DeleteMapping("/{id}")
+	public String delete(@PathVariable("id") int id) {
+		userService.delete(id);
+		return "redirect:/";
+	}
+}
